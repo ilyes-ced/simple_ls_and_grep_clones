371a50bc8fcc23ef01094844b53d6e480ef2a724
ilyes-guy <random_dude_233@proton.me>
2023-03-04 09:49:50 +0100

    test

--- main.c
+++ main.c
@@ -9,14 +9,25 @@ int main() {
 
     printf("personel project \nSQLite3 lite clone\nV 0.0.2");
     while(true){
-        printf("\nSQLone > ");
-        scanf("%s", input);
 
-        if(strcmp(input, ".exit") == 0){
-            exit(EXIT_SUCCESS);
+        printf("\nSQLone > ");
+        fgets (input, 255, stdin);
+
+        //removes the line jump from the input
+        input[strcspn(input, "\n")] = 0;
+
+        if(strcmp(input[0], ".") == 0){
+            switch (do_meta_command(input)) {
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                    continue;
+            }
         }else{
-            printf("%s", input);
+            printf("-> unrecognized command : '%s'", input);
         }
+
     }
   
     return 0;
