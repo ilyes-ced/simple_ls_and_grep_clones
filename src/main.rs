64abd2f3a25cf63367202a999c903df95b15cfe5
ilyes-guy <random_dude_233@proton.me>
2023-03-24 17:41:15 +0100

    start grep

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
-use std::io;
-use std::io::prelude::*;
-use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+use std::env;
 
-fn main() {
-    let stdin = io::stdin();
-    let mut lines = stdin.lock().lines();
-    let mut records = HashMap::new();
 
-    while let Some(label) = lines.next() {
-        let value = lines.next().expect("No value for label");
-        records.insert(label.unwrap(), value.unwrap());
+fn main() -> io::Result<()> {
+    let file = File::open("src/text.txt")?;
+    let reader = BufReader::new(file);
+    //for line in reader.lines() {
+    //    println!("{}", line?);
+    //}
+
+    let mut args: Vec<String> = env::args().collect();
+    
+    let index = args.iter().position(|x| *x == 0).unwrap();
+    args.remove(index);
+    
+
+     //let parts = "some string 123 content".split(" ");
+    //let collection = parts.collect::<Vec<&str>>();
+    
+    print_type_of(&args);
+
+    for arg in args {
+        println!("{}", arg);
     }
+
+    Ok(())
+}
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
 }
