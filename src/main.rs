b94e53533621b0622ebc5833ee5a36c6fab77c04
ilyes-guy <random_dude_233@proton.me>
2023-03-25 10:20:30 +0100

    random idk

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,72 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 use std::env;
+use std::path::PathBuf;
+
+
 
 
 fn main() -> io::Result<()> {
     let file = File::open("src/text.txt")?;
     let reader = BufReader::new(file);
+    let args: Vec<String> = env::args().collect();
+    
+    
+
+
+    for i in 1..args.len() {
+
+
+
+        let para = args[i].split("").collect::<Vec<&str>>();
+        println!("{:?}///", args[i]);
+        //for j in 1..para.len() {
+        //    println!("{:?}", para[j]);
+        //}
+
+        if para[1] == "/" && para[2] == "/" {
+            println!(" /help ");
+        }else if para[1] == "/" {
+            println!(" /h ");
+        }else {
+            println!("other command");
+        }
+
+        let conc = "args[1].".to_owned().spush_str("borrowed_string");
+        println!("{}", conc);
+
+        let file = File::open(&conc)?;
+        let reader = BufReader::new(file);
+        for line in reader.lines() {
+            println!("{}", line?);
+        }
+       
+        //println!("{}", arg);
+        //println!("{:?}", para);
+    }
+
+    //let path = get_current_working_dir();
+    //let path2 = get_current_working_dir();
+    //println!("//////////////////////////////////////////////////////////");
+    //print_type_of(&get_current_working_dir().unwrap().to_str());
+    //print_type_of(&get_current_working_dir());
+    //println!("{:?}", path);
+    //println!("{:?}", path2);
+    //println!("//////////////////////////////////////////////////////////");
+
+
+    
+    let file = File::open("src/text.txt")?;
+    let reader = BufReader::new(file);
     //for line in reader.lines() {
     //    println!("{}", line?);
     //}
-
-    let mut args: Vec<String> = env::args().collect();
-    
-    let index = args.iter().position(|x| *x == 0).unwrap();
-    args.remove(index);
-    
-
-     //let parts = "some string 123 content".split(" ");
-    //let collection = parts.collect::<Vec<&str>>();
-    
-    print_type_of(&args);
-
-    for arg in args {
-        println!("{}", arg);
-    }
-
     Ok(())
 }
 
+fn get_current_working_dir() -> std::io::Result<PathBuf> {
+    env::current_dir()
+}
 fn print_type_of<T>(_: &T) {
     println!("{}", std::any::type_name::<T>())
 }
