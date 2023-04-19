f6e0b5ee91f9630dcf3a48120a2d0c1ea95a5faf
ilyes-guy <random_dude_233@proton.me>
2023-04-19 17:36:52 +0200

    ls

--- src/ls.rs
+++ src/ls.rs
@@ -1,6 +1,47 @@
-use std::{fs, path::PathBuf};
+use std::{fs, path::PathBuf, env};
+
+
+struct arguments{
+    all: bool,
+    recursive: bool,
+    meta: bool,
+    comma: bool,
+    time: bool,
+}
+
+
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let mut args: Vec<String> = env::args().collect();
+    args.remove(0);
+    print!("\x1b[34m{:?}\x1b[0m", args);
+
+
+    //here we proccess the arguments
+    print!("\x1b[34m{:?}\x1b[0m", args.last());
+
+
+
+    match args.last(){
+        Some(arg) => {
+            if &arg[..1] != "-" {
+                return Err("here we make the dir the input or we throw error".into())
+            }
+            for arg in args{
+                println!("{}", arg);
+            }
+        },
+        None => {
+            return Err("no arguments here".into())
+        },
+    }
+
+
+
+
+
+
 
 
 
@@ -15,31 +56,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     };
 
-    for path in paths {
-      
-        match path {
-            Ok(content) => {
-                is_dir_or_file(content.path());
-
-                content
-            }
-            Err(error) => {
-                return Err(error.into());
-            }
-        };
+    let mut sorted_paths: Vec<_> = paths
+        .map(|r| r.unwrap())
+        .collect();
+    
+    sorted_paths.sort_by_key(|dir| dir.path());
 
 
-        //match path {
-        //    Ok(content) => {
-        //        content
-        //    }
-        //    Err(error) => {
-        //        return Err(error.into());
-        //    }
-        //};
+    for path in sorted_paths {
+        is_dir_or_file(path.path());
+    };
 
 
-    }
+    println!("");
 
     Ok(())
 }
@@ -47,19 +76,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 fn is_dir_or_file(file_dir: PathBuf) {
     let is_dir = file_dir.is_dir();
+    let path = &file_dir.to_string_lossy()[2..];
 
     if is_dir {
-        println!("\x1b[34m{}\x1b[0m", file_dir.display());
+        if &path[..1] != "." {
+            print!("\x1b[34m{}\x1b[0m", path);
+            print!("    ");
+        }
     }else{
-        get_file_type(file_dir);
+        if &path[..1] != "." {
+            print!("{}", path);
+            print!("    ");
+        }
     }
 }
 
-
-fn get_file_type(file_dir: PathBuf) {
-    println!("\x1b[32m{}\x1b[0m", file_dir.display());
-}
-
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
-}
